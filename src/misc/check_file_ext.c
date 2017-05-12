3ee3549f63dc58577e74d043c3cb9367d5df998b
Theo GROS <tgros@e2r11p7.42.fr>
2017-05-12 15:36:59 +0200

    file name export bmp auto bmp ext

--- src/misc/check_file_ext.c
+++ src/misc/check_file_ext.c
@@ -6,7 +6,7 @@
 /*   By: tgros <tgros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/08 17:15:14 by tgros             #+#    #+#             */
-/*   Updated: 2017/05/08 17:24:16 by tgros            ###   ########.fr       */
+/*   Updated: 2017/05/12 15:34:42 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ int		check_file_ext(char *file_name, char *ext)
 	char	*ret;
 
 	ft_strtoupper(file_name);
-	if (!(ret = ft_strrchr(file_name, '.')))
+	if (!(ret = ft_strrchr(file_name, '.')) ||
+		ft_strcmp(ret + 1, ext) != 0)
+	{
+		ft_strtolower(file_name);
 		return (0);
-	if (ft_strcmp(ret + 1, ext) != 0)
-		return (0);
+	}
+	ft_strtolower(file_name);
 	return (1);
 }
