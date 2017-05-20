0a3de84713d9c55e53ff9e27d9ea3d0a9458a688
Theo GROS <tgros@e2r8p6.42.fr>
2017-05-20 17:00:28 +0200

    Photon count in settings

--- src/gui/sig_update_settings.c
+++ src/gui/sig_update_settings.c
@@ -6,12 +6,19 @@
 /*   By: tgros <tgros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 11:23:26 by tgros             #+#    #+#             */
-/*   Updated: 2017/05/13 16:23:46 by tgros            ###   ########.fr       */
+/*   Updated: 2017/05/20 17:00:02 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.cuh"
 #include "gui.h"
+
+void	settings_render_sig(t_gtk_tools *g)
+{
+	g->r->update.render = 1;
+	if (g->win)
+		gtk_widget_queue_draw(g->win);
+}
 
 void	*sig_update_tile_size(GtkWidget *spin_button, t_gtk_tools *g)
 {
@@ -23,11 +30,17 @@ void	*sig_update_tile_size(GtkWidget *spin_button, t_gtk_tools *g)
 
 void	*sig_update_aa(GtkWidget *combo_box, t_gtk_tools *g)
 {
-	printf("sig_update_aa");
+	printf("sig_update_aa\n");
 	g->r->scene->is_aa = gtk_combo_box_get_active(GTK_COMBO_BOX(combo_box)) + 1;
-	g->r->update.render = 1;
-	if (g->win)
-		gtk_widget_queue_draw(g->win);
+	(g->updating_gui) ? 0 : settings_render_sig(g);
+	return (NULL);
+}
+
+void	*sig_update_photon_count(GtkWidget *spin_button, t_gtk_tools *g)
+{
+	printf("sig_update_photon_count\n");
+	g->r->scene->photon_count = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spin_button));
+	(g->updating_gui) ? 0 : settings_render_sig(g);
 	return (NULL);
 }
 
