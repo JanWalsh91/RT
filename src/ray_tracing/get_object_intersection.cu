c7e710da1b2764cab271df7b0978012af189d856
Theo GROS <tgros@e1r12p2.42.fr>
2017-06-01 12:37:45 +0200

    Obj intersection

--- src/ray_tracing/get_object_intersection.cu
+++ src/ray_tracing/get_object_intersection.cu
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_obj_intersection.cu                            :+:      :+:    :+:   */
+/*   get_object_intersection.cu                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: talemari <talemari@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tgros <tgros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 15:26:41 by talemari          #+#    #+#             */
-/*   Updated: 2017/05/18 15:28:58 by talemari         ###   ########.fr       */
+/*   Updated: 2017/06/01 12:05:14 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,28 +61,73 @@ bool	get_triangle_intersection_obj(t_raytracing_tools *r, t_triangle *t, t_ray *
 	t_intersection_tools	i;
 	float					tmp;
 
-	i.p = v_sub(t->v2, t->v1);
-	i.q = v_sub(t->v3, t->v1);
-	i.v1 = v_cross(ray->dir, i.q);
-	i.d1 = v_dot(i.p, i.v1);
-	if (i.d1 < 1e-8 && i.d1 > -1e-8)
+	// i.n_dir = 1;
+	// i.p = v_sub(t->v2, t->v1);
+	// i.q = v_sub(t->v3, t->v1);
+	// i.v1 = v_cross(ray->dir, i.q);
+	// i.d1 = v_dot(i.p, i.v1);
+	// if (v_dot(i.v1, ray->dir) > 0)
+	// 	i.n_dir = -1;
+	// i.v1 = v_cross(v_scale(ray->dir, ray->n_dir), i.q);
+	// i.d1 = v_dot(i.p, i.v1);
+	// if (i.d1 > -1e-20)
+	// 	return (false);
+	// i.d2 = 1 / i.d1;
+	// i.v2 = v_sub(ray->origin, t->v1);
+	// i.r1 = v_dot(v_scale(i.v1, i.n_dir), i.v2) * i.d2;
+	// if (i.r1 < 0)
+	// 	return (false);
+
+	i.v1 = v_sub(t->v2, t->v1);
+	i.v2 = v_sub(t->v3, t->v2);
+	i.v3 = v_sub(t->v1, t->v3);
+	t_vec3	normale = v_norm(v_cross(i.v1, i.v3));
+
+	if (r->idx == 0)
+	printf("Normale [:] %f, %f, %f\n", normale.x, normale.y, normale.z);
+
+	i.n_dir = 1;
+	if (v_dot(normale, ray->dir) > 0)
+		i.n_dir = -1;
+	i.d1 = v_dot(v_scale(normale, i.n_dir), ray->dir);
+	if (i.d1 > -1e-20)
 		return (false);
-	i.d2 = 1 / i.d1;
-	i.v2 = v_sub(ray->origin, t->v1);
-	i.r1 = v_dot(i.v1, i.v2) * i.d2;
-	if (i.r1 < 0 || i.r1 > 1)
+	i.p = v_sub(t->v1, ray->origin);
+	i.r1 = v_dot(i.p, v_scale(normale, i.n_dir)) / i.d1;
+	if (i.r1 < 0)
 		return (false);
-	i.v3 = v_cross(i.v2, i.p);
-	i.r2 = v_dot(ray->dir, i.v3) * i.d2;
-	if (i.r2 < 0 || i.r1 + i.r2 > 1)
+	//fin de verif avec plan intersection
+	
+
+	i.p = v_add(ray->origin, v_scale(ray->dir, i.r1));
+	if (v_dot(v_scale(normale, i.n_dir), v_cross(i.v1, v_sub(i.p, t->v1))) < 0.0 ||
+		v_dot(v_scale(normale, i.n_dir), v_cross(i.v2, v_sub(i.p, t->v2))) < 0.0 ||
+		v_dot(v_scale(normale, i.n_dir), v_cross(i.v3, v_sub(i.p, t->v3))) < 0.0)
 		return (false);
-	tmp = v_dot(i.q, i.v3) * i.d2;
-	if (tmp < ray->t)
+
+
+	r->t > i.r1 ? ray->t = i.r1 : 0.0;
+	if (r->t > i.r1)
 	{
-		ray->t = tmp;
+		// ray->hit_obj = index;
+		ray->nhit = normale;
+		ray->hit_type = T_PLANE;
 		if (v_dot(ray->nhit, ray->dir) > 0)
 			ray->n_dir = -1;
 	}
+
+	// i.v3 = v_cross(i.v2, i.p);
+	// i.r2 = v_dot(ray->dir, i.v3) * i.d2;
+	// if (i.r2 < 0 || i.r1 + i.r2 > 1)
+	// 	return (false);
+	// tmp = v_dot(i.q, i.v3) * i.d2;
+	// r->t > tmp ? ray->t = tmp : 0.0;
+	// if (r->t > tmp)
+	// {
+	// 	ray->t = tmp;
+	// 	if (v_dot(ray->nhit, ray->dir) > 0)
+	// 		ray->n_dir = -1;
+	// }
 	return (true);
 }
 
@@ -99,9 +144,10 @@ bool	get_obj_intersection(t_raytracing_tools *r, t_ray *ray, int index)
 	// if (r->idx != 0)
 	// 	return (0);
 	raytmp = ray->t;
-	printf("obj inter\n");
-	if (!(get_sphere_intersection_obj(r, ray, index)))
-		return (false);
+	ray->n_dir = 1;
+	// printf("obj inter\n");
+	//if (!(get_sphere_intersection_obj(r, ray, index)))
+	//	return (false);
 	t = INFINITY;
 	ray->t = raytmp;
 	o = r->scene->objects[index].obj;
@@ -119,12 +165,16 @@ bool	get_obj_intersection(t_raytracing_tools *r, t_ray *ray, int index)
 		triangle.v3 = o->vertex[lst_triangle->v.z];
 		if (get_triangle_intersection_obj(r, &triangle, ray))
 		{
-			if (ray->t < t)
-			{
-				t = ray->t;
-				ray->nhit = v_cross(v_sub(triangle.v2, triangle.v1),
-						v_sub(triangle.v3, triangle.v1));
-			}
+			if (r->t > ray->t)
+				r->t = ray->t;
+			// if (ray->t < t)
+			// {
+			// 	t = ray->t;
+			// 	ray->nhit = v_cross(v_sub(triangle.v2, triangle.v1),
+			// 			v_sub(triangle.v3, triangle.v1));
+			// 	if (v_dot(ray->nhit, ray->dir) > 0)
+			// 		ray->n_dir = -1;
+			// }
 		}
 		//if (r->pix.x == 600 && r->pix.y < 200)
 			//printf("addr %p\n", o->triangle->next);
