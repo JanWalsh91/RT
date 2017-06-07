9a6e78260260f2a174561176be7488b2af8977a0
Jan WALSH <jwalsh@e2r9p4.42.fr>
2017-06-07 14:37:46 +0200

    workedo n ppm

--- src/gui/sig_render.c
+++ src/gui/sig_render.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 16:43:54 by tgros             #+#    #+#             */
-/*   Updated: 2017/06/07 14:08:20 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/06/07 14:36:44 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,6 +111,7 @@ void		*render_wrapper(gpointer data)
 				g->r->scene->res.x, g->r->scene->res.y);
 		gdk_pixbuf_fill(g->pixbuf, 0);
 	}
+	return (NULL);
 	cuda_malloc(g->r);
 	malloc_region_map(g->r, tile);
 	cuda_malloc_region_map_tile(g->r, tile);
