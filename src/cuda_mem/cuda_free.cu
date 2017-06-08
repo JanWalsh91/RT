99f0ebbe4320efef43c96f2d30393ffc8953c8cb
Jan WALSH <jwalsh@e2r8p5.42.fr>
2017-06-08 20:51:52 +0200

    added cuda free protections

--- src/cuda_mem/cuda_free.cu
+++ src/cuda_mem/cuda_free.cu
@@ -6,7 +6,7 @@
 /*   By: jwalsh <jwalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/22 14:56:48 by tgros             #+#    #+#             */
-/*   Updated: 2017/06/08 14:14:09 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/06/08 20:50:59 by jwalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,18 @@
 
 int	cuda_free(t_raytracing_tools *r, int all)
 {
-	if (r->update.resolution == 2 || all)
+	if ((r->update.resolution == 2 || all) && r->d_pixel_map)
 		cudaFree(r->d_pixel_map);
-	if (r->update.objects == 2 || all)
-		cudaFree(r->h_d_scene->objects);
-	if (r->update.lights == 2 || all)
-		cudaFree(r->h_d_scene->lights);
-	if (r->update.cameras == 2 || all)
-		cudaFree(r->h_d_scene->cameras);
-	if (r->update.scene == 2 || all)
+	if (r->h_d_scene)
+	{
+		if ((r->update.objects == 2 || all) && r->h_d_scene->objects)
+			cudaFree(r->h_d_scene->objects);
+		if ((r->update.lights == 2 || all) && r->h_d_scene->lights)
+			cudaFree(r->h_d_scene->lights);
+		if ((r->update.cameras == 2 || all) && r->h_d_scene->cameras)
+			cudaFree(r->h_d_scene->cameras);
+	}
+	if ((r->update.scene == 2 || all) && r->d_scene)
 		cudaFree(r->d_scene);
 	return (0);
 }
